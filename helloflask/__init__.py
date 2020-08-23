5c892359c9861b2eb57b81499f79272897d29ad0
CodyMoon <36313353+CodyMoon@users.noreply.github.com>
2020-08-24 00:18:58 +0900

    wav file error

--- helloflask/__init__.py
+++ helloflask/__init__.py
@@ -1,36 +1,34 @@
-from datetime import datetime
-
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request,redirect
+import speech_recognition as sr
 
 app = Flask(__name__)
 
-posts = [
-    {
-        'author': {
-            'username': 'test-user'
-        },
-        'title': '첫 번째 아니아니',
-        'content': '첫 번째 아니 내용입니다.',
-        'date_posted': datetime.strptime('2018-08-01', '%Y-%m-%d')
-    },
-    {
-        'author': {
-            'username': 'test-user'
-        },
-        'title': '세 번째 포스트',
-        'content': '두 번째 포스트 내용입니다.',
-        'date_posted': datetime.strptime('2018-08-03', '%Y-%m-%d')
-    },
-]
-
 @app.route('/')
-def mainPage():
-    return render_template('index.html', posts=posts)
-
-@app.route('/index')
 def index():
-    return render_template('dropFile.html', title='File')
+    return render_template('index.html')
+
+@app.route('/upload', methods=["GET" , "POST"])
+def upload():
+    transcript=""
+    if request.method == "POST":
+        print("FORM DATA RECEIVED")
+
+        if "file" not in request.files:
+            return redirect(request.url)
+
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+        
+        if file:
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as source:
+                data = recognizer.record(source)
+            transcript = recognizer.recognize_google(data, key=None)
+
+    return render_template('upload.html', transcript = transcript)
 
 @app.route('/recording')
 def recording():
-  return render_template('recording.html', title='Recording')
+    return render_template('recording.html')
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 from helloflask import app
 
-app.run(host='0.0.0.0')
+if __name__=="__main__":
+    app.run(debug=True)
